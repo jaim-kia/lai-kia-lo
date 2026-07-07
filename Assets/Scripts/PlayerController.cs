717e6440a399065d5bceb2639f8448286cc627b7
jam <jaim.tan@student.ateneo.edu>
2026-07-07 21:00:04 +0800

    [Feature Development] Added Cayote Time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,13 +13,22 @@ public class PlayerController : MonoBehaviour
     [Header("Grounding")]
     [SerializeField] LayerMask groundLayer;
 
+    [Header("Responsiveness")]
+    [SerializeField] float cayoteTime = 0.15f;
+    [SerializeField] float jumpBufferTime = 0.15f;
+
     private float horizontal;
     private bool grounded = false;
+
+    private float cayoteTimeCounter;
+    private float jumpBufferCounter;
 
     public static PlayerController Instance;
     
     private void Awake()
     {
+        Physics.gravity = new Vector3(0, -19.6f, 0);
+
         if(Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -27,6 +36,24 @@ public class PlayerController : MonoBehaviour
         else
         {
             Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        if (grounded)
+            cayoteTimeCounter = cayoteTime;
+        else
+            cayoteTimeCounter -= Time.deltaTime;
+
+        jumpBufferCounter -= Time.deltaTime;
+
+        if (jumpBufferCounter > 0f && cayoteTimeCounter > 0f)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpingPower, 0);
+
+            jumpBufferCounter = 0f;
+            cayoteTimeCounter = 0f; 
         }
     }
 
@@ -42,12 +69,12 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.performed && grounded)
+        if(context.performed)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpingPower, 0);
+            jumpBufferCounter = jumpBufferTime;
         }
         else if (context.canceled && rb.linearVelocity.y > 0) {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, 0);
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y/2, 0);
         }
     }
 
