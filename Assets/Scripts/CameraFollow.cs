0dbd04536d68564f289d91bac3317b87eb77d6a0
jam <jaim.tan@student.ateneo.edu>
2026-08-30 20:47:42 +0800

    [Feature Development] Camera Horizontal Bias
    Commit Right before Camera Overhaul

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,24 +8,38 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float nudgeAmount = 10f;
     [SerializeField] private float nudgeSmoothTime = 0.3f;
 
+    [SerializeField] private Transform lookAhead;
+    private float horizontalNudge = 1.0f;
+    private float currentHorizontalNudge = 1.0f;
+
     private float currentVerticalNudge;
     private float nudgeVelocity;
+    private float nudgeHVelocity;
 
     // Update is called once per frame
     void LateUpdate()
     {
         float verticalInput = PlayerController.CameraMoveValue;
-        float targetNudge = (Mathf.Abs(verticalInput) > 0.5f) ? verticalInput * nudgeAmount : 0f;
+        float targetVerticalNudge = (Mathf.Abs(verticalInput) > 0.5f) ? verticalInput * nudgeAmount : 0f;
 
         currentVerticalNudge = Mathf.SmoothDamp(
-            currentVerticalNudge, targetNudge, ref nudgeVelocity, nudgeSmoothTime);
+            currentVerticalNudge, targetVerticalNudge, ref nudgeVelocity, nudgeSmoothTime);
+
+        var facing = PlayerController.Instance.Facing;
+        if (facing == PlayerController.FacingDirection.Right)
+            horizontalNudge = 1.0f;
+        else
+            horizontalNudge = -1.0f;
+        
+        currentHorizontalNudge = Mathf.SmoothDamp(
+            currentHorizontalNudge, horizontalNudge, ref nudgeHVelocity, nudgeSmoothTime);
 
         Vector3 playerPos = PlayerController.Instance.transform.position;
 
         transform.position = new Vector3(
-            playerPos.x + offset.x,
-            playerPos.y + offset.y + currentVerticalNudge,
-            playerPos.z + offset.z 
+            lookAhead.position.x + offset.x + currentHorizontalNudge,
+            lookAhead.position.y + offset.y + currentVerticalNudge,
+            lookAhead.position.z + offset.z 
         );
     }
 }
